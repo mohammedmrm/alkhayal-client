2e5f9cdc0520b9a7a3fb10cb7af1a507740bd093
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2023-09-24 11:43:48 +0200

    name tryo

--- app/config/settings.js
+++ app/config/settings.js
@@ -3,17 +3,17 @@ const settings = {
   dev: {
     apiUrl: "http://ec2-3-137-158-198.us-east-2.compute.amazonaws.com/client/api",
     logo: require("../assets/logo/logo.png"),
-    companyName: "شركة السرعه",
+    companyName: "شركة سرعه",
   },
   staging: {
     apiUrl: "http://ec2-3-137-158-198.us-east-2.compute.amazonaws.com/client/api",
     logo: require("../assets/logo/logo.png"),
-    companyName: "شركة السرعه",
+    companyName: "شركة سرعه",
   },
   prod: {
     apiUrl: "http://ec2-3-137-158-198.us-east-2.compute.amazonaws.com/client/api",
     logo: require("../assets/logo/logo.png"),
-    companyName: "شركة السرعه",
+    companyName: "شركة سرعه",
   },
 };
 
