42a28806e3edabfa637c96f0baade6d2a628e47d
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2023-10-28 00:13:49 +0200

    re init for almalk

--- app/config/settings.js
+++ app/config/settings.js
@@ -1,26 +1,18 @@
 import Constants from "expo-constants";
 const settings = {
   dev: {
-    apiUrl: "https://alamenexpress.com/client/api",
+    apiUrl: "http://almalkexpress.com/client/api",
     logo: require("../assets/logo/logo.png"),
-    companyName: "جوهرة الشمال",
+    companyName: "الملك",
   },
   staging: {
-    apiUrl: "https://alamenexpress.com/client/api",
+    apiUrl: "http://almalkexpress.com/client/api",
     logo: require("../assets/logo/logo.png"),
-    companyName: "جوهرة الشمال",
+    companyName: "الملك",
   },
   prod: {
-    apiUrl: "https://alamenexpress.com/client/api",
+    apiUrl: "http://almalkexpress.com/client/api",
     logo: require("../assets/logo/logo.png"),
-    companyName: "جوهرة الشمال",
+    companyName: "الملك",
   },
 };
-
-const getCurrentSettings = () => {
-  if (__DEV__) return settings.dev;
-  if (Constants.manifest.releaseChannel === "staging") return settings.staging;
-  return settings.prod;
-};
-
-export default getCurrentSettings();
