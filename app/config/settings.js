683bc00635a7afa5c516b60ce136407c4679ed45
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2023-10-28 00:24:32 +0200

    fix typo

--- app/config/settings.js
+++ app/config/settings.js
@@ -1,5 +1,5 @@
 import Constants from "expo-constants";
-const settings = {
+export const settings = {
   dev: {
     apiUrl: "http://almalkexpress.com/client/api",
     logo: require("../assets/logo/logo.png"),
@@ -16,3 +16,10 @@ const settings = {
     companyName: "الملك",
   },
 };
+const getCurrentSettings = () => {
+  if (__DEV__) return settings.dev;
+  if (Constants.manifest.releaseChannel === "staging") return settings.staging;
+  return settings.prod;
+};
+
+export default getCurrentSettings();
