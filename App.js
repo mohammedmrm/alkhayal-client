06adf242dd4d1acb0386d6412f6fedfbbdf4af00
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2023-11-18 17:32:24 +0100

    clean up

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { ApplicationProvider } from "@ui-kitten/components";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import React, { useCallback, useEffect, useState } from "react";
-import { SafeAreaView } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import AuthContext from "./app/auth/context";
 import authStorage from "./app/auth/storage";
@@ -13,7 +12,6 @@ import AppNavigator from "./app/navigations/AppNavigation";
 import AuthNavigator from "./app/navigations/AuthNavigator";
 import navigationTheme from "./app/navigations/NavigationTheme";
 import { navigationRef } from "./app/navigations/rootNavigation";
-import ActivityIndecator from "./app/components/ActivtyIndectors/ActivityIndecatorChatLoading";
 
 export default function App() {
   const [user, setUser] = useState();
@@ -28,46 +26,32 @@ export default function App() {
     Tjw_Ereg: require("./app/assets/fonts/Cairo-ExtraLight.ttf"),
   });
   const restoreUser = async () => {
-    authStorage.getUser().then((user) => {
-      if (user && user.code && user.code !== "300") {
-        setUser(user);
-      }
-    });
+    authStorage
+      .getUser()
+      .then((user) => {
+        if (user && user.code && user.code !== "300") {
+          setUser(user);
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+      })
+      .finally(() => setIsReady(true));
   };
   useEffect(() => {
-    const prepare = () => {
-      restoreUser()
-        .catch((e) => console.log(e))
-        .finally(() => {
-          setIsReady(true);
-        });
-    };
-    prepare();
+    restoreUser();
   }, []);
 
   const onLayoutRootView = useCallback(async () => {
-    if (isReady) await SplashScreen.hideAsync();
-  }, [isReady]);
+    if (isReady && loaded) await SplashScreen.hideAsync();
+  }, [isReady, loaded]);
 
   useEffect(() => {
     onLayoutRootView();
   }, [isReady]);
 
   if (!isReady || !loaded) {
-    return (
-      <SafeAreaView
-        style={{
-          width: "100%",
-          alignSelf: "center",
-          justifyContent: "center",
-          alignItems: "center",
-          overflow: "hidden",
-          flex: 1,
-        }}
-      >
-        <ActivityIndecator visible={true} />
-      </SafeAreaView>
-    );
+    return null;
   }
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
